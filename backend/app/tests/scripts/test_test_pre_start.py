8aea28f8622dec65e50fa391ceb131559efff74c
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-10-18 16:17:57 +0200

    fix db tests

--- backend/app/tests/scripts/test_test_pre_start.py
+++ backend/app/tests/scripts/test_test_pre_start.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock, patch
 
-# from sqlmodel import select
+from sqlmodel import select
+
 from app.tests_pre_start import init, logger
 
 
@@ -8,11 +9,13 @@ def test_init_successful_connection() -> None:
     engine_mock = MagicMock()
 
     session_mock = MagicMock()
-    exec_mock = MagicMock(return_value=True)
-    session_mock.configure_mock(**{"exec.return_value": exec_mock})
+    session_mock.__enter__.return_value = session_mock
+
+    select1 = select(1)
 
     with (
-        patch("sqlmodel.Session", return_value=session_mock),
+        patch("app.tests_pre_start.Session", return_value=session_mock),
+        patch("app.tests_pre_start.select", return_value=select1),
         patch.object(logger, "info"),
         patch.object(logger, "error"),
         patch.object(logger, "warn"),
@@ -27,7 +30,4 @@ def test_init_successful_connection() -> None:
             connection_successful
         ), "The database connection should be successful and not raise an exception."
 
-        # Todo: fix this
-        # assert session_mock.exec.called_once_with(
-        #     select(1)
-        # ), "The session should execute a select statement once."
+        session_mock.exec.assert_called_once_with(select1)
