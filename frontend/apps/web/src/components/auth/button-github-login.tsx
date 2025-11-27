576faf71491ada134b7d96cb274603a3bf3960fb
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-11-27 22:29:52 +0100

    try in page

--- frontend/apps/web/src/components/auth/button-github-login.tsx
+++ frontend/apps/web/src/components/auth/button-github-login.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { connection } from 'next/server';
 
 import IconsCustom from '@workspace/ui/components/icons-custom';
 import { Button } from '@workspace/ui/components/ui/button';
@@ -20,28 +19,21 @@ const { NEXT_PUBLIC_API_URL } = PROCESS_ENV;
 const { LOGIN_GITHUB } = ROUTES.API;
 
 // Note: BROWSER (<a/> or <Link />) must call this API url, not http client or server
-const _absoluteApiUrl = `${NEXT_PUBLIC_API_URL}${LOGIN_GITHUB}`;
+const absoluteApiUrl = `${NEXT_PUBLIC_API_URL}${LOGIN_GITHUB}`;
 
-const ButtonGithubLogin: FC<Props> = async ({ className }) => {
-  // For runtime env vars
-  await connection();
-
-  const absoluteApiUrl = `${process.env.NEXT_PUBLIC_API_URL}${LOGIN_GITHUB}`;
-
-  return (
-    <Button
-      asChild
-      className={cn(
-        'w-full h-12 flex items-center justify-center gap-2 rounded-md font-medium text-white bg-green-600 hover:bg-green-700 active:bg-green-800 transition-colors',
-        className
-      )}
-    >
-      <Link href={absoluteApiUrl}>
-        <GitHub className="w-5 h-5" />
-        Sign in with GitHub
-      </Link>
-    </Button>
-  );
-};
+const ButtonGithubLogin: FC<Props> = async ({ className }) => (
+  <Button
+    asChild
+    className={cn(
+      'w-full h-12 flex items-center justify-center gap-2 rounded-md font-medium text-white bg-green-600 hover:bg-green-700 active:bg-green-800 transition-colors',
+      className
+    )}
+  >
+    <Link href={absoluteApiUrl}>
+      <GitHub className="w-5 h-5" />
+      Sign in with GitHub
+    </Link>
+  </Button>
+);
 
 export default ButtonGithubLogin;
--- frontend/apps/web/src/instrumentation.ts
+++ frontend/apps/web/src/instrumentation.ts
@@ -2,13 +2,9 @@
 
 /** Log loaded env vars. */
 export const register = async () => {
-  console.log('process.env.NEXT_PUBLIC_API_URL 1', process.env.NEXT_PUBLIC_API_URL);
-
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { prettyPrintObject } = await import('@/utils/log');
     const { PROCESS_ENV } = await import('@/config/process-env');
-
-    console.log('process.env.NEXT_PUBLIC_API_URL 2', process.env.NEXT_PUBLIC_API_URL);
 
     prettyPrintObject(PROCESS_ENV, 'parsed PROCESS_ENV');
   }
