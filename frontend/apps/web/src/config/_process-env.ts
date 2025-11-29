a2cf61dd53acf13f701fbc821d00e8970576539d
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-11-29 12:18:12 +0100

    log server only _PROCESS_ENV in instrumentation

---
+++ frontend/apps/web/src/config/_process-env.ts
@@ -0,0 +1,15 @@
+import 'server-only'; // should be for runtime env vars
+
+import { processEnvSchema } from '@/schemas/config';
+import { validateData } from '@/utils/validation';
+
+import { ProcessEnvType } from '@/types/config';
+
+// Note: maybe function for runtime vars
+const processEnvData: ProcessEnvType = {
+  NODE_ENV: process.env.NODE_ENV,
+  NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL,
+  NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+};
+
+export const _PROCESS_ENV = validateData(processEnvData, processEnvSchema);
