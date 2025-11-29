a041ea6df00c49e9f65a064b0515da46044474c5
nemanjam <nemanja.mitic.elfak@hotmail.com>
2025-11-29 12:28:47 +0100

    undo process env

--- frontend/apps/web/src/instrumentation.ts
+++ frontend/apps/web/src/instrumentation.ts
@@ -5,9 +5,7 @@ export const register = async () => {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     const { prettyPrintObject } = await import('@/utils/log');
     const { PROCESS_ENV } = await import('@/config/process-env');
-    const { _PROCESS_ENV } = await import('@/config/_process-env');
 
     prettyPrintObject(PROCESS_ENV, 'parsed PROCESS_ENV');
-    prettyPrintObject(_PROCESS_ENV, 'parsed _PROCESS_ENV');
   }
 };
